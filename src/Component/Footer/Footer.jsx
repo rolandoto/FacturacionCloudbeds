08ae98abe01d110005aa0587bcdf6c35503eeda7
Rolando Guerrero <rolando22_@outlook.com>
2024-10-22 20:47:24 +0000

    add update commit

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,7 +2,7 @@ export function Footer () {
     return (
       <footer className='footer'>
         <img src="https://cloudbeds-fcfc.kxcdn.com/wp-content/uploads/2021/04/CB_logo.svg" alt="" />
-        <h4>cloudbeds  @Rolando Guerrero</h4>
+        <h4>cloudbeds  10 elementos</h4>
       </footer>
     )
   }
